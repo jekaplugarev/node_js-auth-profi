f22ad0598341a865a11119ccaeb1fb7fb56f83da
jekaplugarev <jeka_plugarev@mail.ru>
2022-01-26 17:46:43 +0800

    initial commit

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -59,7 +59,7 @@ class UserController {
     async refresh(req, res, next) {
         try {
             const {refreshToken} = req.cookies
-            const userData = await userService.refresh(email, password)
+            const userData = await userService.refresh(refreshToken)
             res.cookie('refreshToken', userData.refreshToken, {
                 maxAge: 30 * 24 * 60 * 60 * 1000, // Время жизни куки 30 дней в миллисекундах
                 httpOnly: true
@@ -72,7 +72,8 @@ class UserController {
 
     async getUsers(req, res, next) {
         try {
-            res.json(['111', '222'])
+            const users = await userService.getAllUsers()
+            return res.json(users)
         } catch (e) {
             next(e)
         }
